914ce2153df382173fdff2360c2fe231992a6136
Knud Möller <knud@datalysator.com>
2018-04-26 15:22:50 +0200

    implement IConfigurer to set dcat config settings from within plugin

--- ckanext/dcatde_berlin/plugin.py
+++ ckanext/dcatde_berlin/plugin.py
@@ -1,4 +1,18 @@
+# coding: utf-8
+
+import os
 import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
 
 class Dcatde_BerlinPlugin(plugins.SingletonPlugin):
-  pass
+
+    plugins.implements(plugins.IConfigurer, inherit=False)
+
+    # -------------------------------------------------------------------
+    # Implementation IConfigurer
+    # -------------------------------------------------------------------
+
+    def update_config(self, config):  
+        config['ckanext.dcat.enable_content_negotiation'] = True
+        config['ckanext.dcat.rdf.profiles'] = "euro_dcat_ap dcatap_de"
+        config['ckanext.dcatde.contributorid'] = "berlinOpenData"
