b5a1b93b9dbf83a3110e7f10249e0a46d0003730
Knud Möller <knud.moeller@berlinonline.de>
2025-03-30 10:33:26 +0200

    change path to CKAN_INI

--- run_single_test.sh
+++ run_single_test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export CKAN_INI="/usr/lib/ckan/default/src/ckan/test-core.ini"
+export CKAN_INI="../../src/ckan/test-core.ini"
 
 # delete .pyc-files to prevent the "import file mismatch" errors
 find -name "*.pyc" -delete
