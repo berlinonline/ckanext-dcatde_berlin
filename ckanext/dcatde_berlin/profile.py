382040520569da1d4322647faf2da35300d2e342
Knud Möller <knud.moeller@berlinonline.de>
2025-05-10 00:36:45 +0200

    use librdf's namespace definitions instead of our own wherever possible

--- ckanext/dcatde_berlin/profile.py
+++ ckanext/dcatde_berlin/profile.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse
 
 from ckan.common import config
 from rdflib import BNode, Graph, Literal, Namespace, URIRef
-from rdflib.namespace import RDF, RDFS, DCTERMS
+from rdflib.namespace import DCAT, DCTERMS, FOAF, OWL, RDF, RDFS, SKOS, TIME, VOID
 
 from ckanext.dcat.profiles import RDFProfile
 from ckanext.dcat.utils import resource_uri
@@ -15,18 +15,12 @@ from ckanext.dcat.utils import resource_uri
 LOG = logging.getLogger(__name__)
 
 # copied from ckanext.dcat.profiles
-DCAT = Namespace("http://www.w3.org/ns/dcat#")
 ADMS = Namespace("http://www.w3.org/ns/adms#")
 VCARD = Namespace("http://www.w3.org/2006/vcard/ns#")
-FOAF = Namespace("http://xmlns.com/foaf/0.1/")
 SCHEMA = Namespace('http://schema.org/')
-TIME = Namespace('http://www.w3.org/2006/time')
 LOCN = Namespace('http://www.w3.org/ns/locn#')
 GSP = Namespace('http://www.opengis.net/ont/geosparql#')
-OWL = Namespace('http://www.w3.org/2002/07/owl#')
 SPDX = Namespace('http://spdx.org/rdf/terms#')
-SKOS = Namespace('http://www.w3.org/2004/02/skos/core#')
-VOID = Namespace('http://rdfs.org/ns/void#')
 DCATAP = Namespace('http://data.europa.eu/r5r/')
 
 # own namespaces
